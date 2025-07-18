2d45624dd88cbe13278d8bd24600a6e3d87e362f
unknown <lakshaygupta2511@gmail.com>
2025-07-18 21:50:54 +0530

    Fixed the logo and tagline

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "DevilDev",
-  description: "Cooking something wicked...",
+  description: "The First Engineer",
   icons: {
     icon: "/favicon.png",
   },
