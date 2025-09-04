a6047b7b70f1f791d0fcd9bbf2a05277d1e1f1b6
lak7 <lakshaygupta2511@gmail.com>
2025-09-04 12:18:16 +0530

    Fixed the build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,6 +39,24 @@ interface GitHubStatus {
   connectedAt?: Date;
 }
 
+function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+
+    const listener = () => setMatches(media.matches);
+    media.addEventListener('change', listener);
+
+    return () => media.removeEventListener('change', listener);
+  }, [matches, query]);
+
+  return matches;
+}
+
 
 export default function Page() {
   const [inputMessage, setInputMessage] = useState('');
@@ -595,20 +613,4 @@ export default function Page() {
   );
 }
 
-export function useMediaQuery(query: string): boolean {
-  const [matches, setMatches] = useState(false);
 
-  useEffect(() => {
-    const media = window.matchMedia(query);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
-    }
-
-    const listener = () => setMatches(media.matches);
-    media.addEventListener('change', listener);
-
-    return () => media.removeEventListener('change', listener);
-  }, [matches, query]);
-
-  return matches;
-}
